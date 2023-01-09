08d6954ac9184b4ea82fb57811d3aa259188c7fe
Lamisa Saadat <ls.lamisa12@gmail.com>
2023-01-09 11:54:02 -0500

    Added a delete button to each todo item. Added a handler to remove todo item from the state when clicked. Added the function in the interface to be received as props in the todolist component

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,13 +3,19 @@ import React from "react";
 //tell TS the type of props this component is receiving
 interface TodoListProps {
   items: { id: string; text: string }[];
+  onDeleteTodo: (id: string) => void;
 }
 
 export const TodoList: React.FC<TodoListProps> = (props) => {
   return (
     <ul>
       {props.items.map((todo) => (
-        <li key={todo.id}>{todo.text}</li>
+        <li key={todo.id}>
+          <span>{todo.text}</span>
+          <button onClick={props.onDeleteTodo.bind(null, todo.id)}>
+            Delete
+          </button>
+        </li>
       ))}
     </ul>
   );
