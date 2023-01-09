efc9b2ac2c2c9314fe5d93da36d09ab92ab7f283
Lamisa Saadat <ls.lamisa12@gmail.com>
2023-01-09 11:35:46 -0500

    Created a form component to add new todos. Created a type in the newtodo component to pass down a function as a prop

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,26 @@
+import React, { useRef } from "react";
+
+type NewTodoProps = {
+  onAddTodo: (todoText: string) => void;
+};
+
+export const NewTodo: React.FC<NewTodoProps> = (props) => {
+  //useRef is a generic function. We want to know which kind of data will be stored inside the ref. Provide nul as default value b/c when the code executes so that it doesn't point at anything
+  const textInputRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const enteredText = textInputRef.current!.value;
+    props.onAddTodo(enteredText);
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      <div>
+        <label htmlFor="todo-text">TO DO</label>
+        <input type="text" id="todo-text" ref={textInputRef} />
+        <button type="submit">ADD TODO</button>
+      </div>
+    </form>
+  );
+};
